335d1f2a78cdee1612d0d2b1730325c563acbaaa
y-matsuda <yuri.matsuda@change-vision.com>
2016-01-18 14:09:58 +0900

    TextArea size of the log was increased.

--- src/main/java/excel2er/ui/ImportDialogBase.java
+++ src/main/java/excel2er/ui/ImportDialogBase.java
@@ -116,7 +116,7 @@ public abstract class ImportDialogBase extends JDialog{
 
 		String detailMessage = result.getMessage();
 		if (StringUtils.isNotEmpty(detailMessage)) {
-			JTextArea detailTextArea = new JTextArea(detailMessage, 5, 10);
+            JTextArea detailTextArea = new JTextArea(detailMessage, 25, 60);
 			detailTextArea.setName(DETAIL_TEXT);
 			detailTextArea.setAlignmentY(Component.BOTTOM_ALIGNMENT);
 			detailTextArea.setAlignmentX(Component.CENTER_ALIGNMENT);

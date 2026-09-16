154299f9c22566202b0c06cb60fe94451202eb02
harry <harry@localhost>
2026-09-16 14:25:42 +0800

    POIを5.5.1へ上げ、OSGi配線と破壊的API変更に対応して、バージョンを1.3.0にする

--- src/main/java/excel2er/services/ParseExcelToDomainModelService.java
+++ src/main/java/excel2er/services/ParseExcelToDomainModelService.java
@@ -1,5 +1,6 @@
 package excel2er.services;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -16,27 +17,27 @@ import excel2er.services.parser.ParserUtils;
 public class ParseExcelToDomainModelService {
 
 	public List<Domain> parse(DomainConfiguration configuration) {
-		Workbook wb = ParserUtils.getWorkbook(configuration);
+        try (Workbook wb = ParserUtils.getWorkbook(configuration)) {
+            Sheet sheet = null;
+            try {
+                if (!StringUtils.isEmpty(configuration.getSheetName())) {
+                    sheet = wb.getSheet(configuration.getSheetName());
+                } else {
+                    sheet = wb.getSheetAt(0);
+                }
+            } catch (Exception e) {
+                throw new ApplicationException(Messages.getMessage("error.not_found_sheet"));
+            }
 
-		Sheet sheet = null;
-		try{
-			if(!StringUtils.isEmpty(configuration.getSheetName())){
-				sheet = wb.getSheet(configuration.getSheetName());
-			}else{
-				sheet = wb.getSheetAt(0);
-			}
-		}catch(Exception e){
-			throw new ApplicationException(Messages.getMessage("error.not_found_sheet"));
-		}
-		
-		if(sheet == null){
-			throw new ApplicationException(Messages.getMessage("error.not_found_sheet"));
-		}
-		
-		DomainParser domainParser = new DomainParser();
-		List<Domain> domains = domainParser.parse(configuration, sheet);
+            if (sheet == null) {
+                throw new ApplicationException(Messages.getMessage("error.not_found_sheet"));
+            }
 
-		return domains;
+            DomainParser domainParser = new DomainParser();
+            return domainParser.parse(configuration, sheet);
+        } catch (IOException e) {
+            throw new ApplicationException(e);
+        }
 	}
 
 }
